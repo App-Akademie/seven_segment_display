80b9a1ff1b2cb652958a4775e4a828f7e1e5f8c3
LukasAppAkademie <lukas.buck@app-akademie.com>
2024-06-01 09:50:25 +0200

    refactored exectution speed function

---
+++ lib/seven_segment_display/domain/execution_speed_model.dart
@@ -0,0 +1,8 @@
+import 'package:seven_segement_display/seven_segment_display/domain/executution_speed.dart';
+
+class ExecutionSpeedData {
+  final ExecutionSpeed speed;
+  final String label;
+
+  ExecutionSpeedData(this.speed, this.label);
+}
--- lib/seven_segment_display/presentation/widgets/execution_speed_switch.dart
+++ lib/seven_segment_display/presentation/widgets/execution_speed_switch.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:seven_segement_display/common/controller/segment_controller.dart';
+import 'package:seven_segement_display/seven_segment_display/domain/execution_speed_model.dart';
 import 'package:seven_segement_display/seven_segment_display/domain/executution_speed.dart';
 
 class ExecutionSpeedSwitch extends StatelessWidget {
@@ -8,33 +9,32 @@ class ExecutionSpeedSwitch extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final segmentController = Provider.of<SegmentController>(context);
+    final speeds = [
+      ExecutionSpeedData(ExecutionSpeed.halfSpeed, "x0.5"),
+      ExecutionSpeedData(ExecutionSpeed.normalSpeed, "x1"),
+      ExecutionSpeedData(ExecutionSpeed.doubleSpeed, "x2"),
+    ];
+
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        _buildExecutionSpeedElement(ExecutionSpeed.halfSpeed, context, "x0.5"),
-        _buildExecutionSpeedElement(ExecutionSpeed.normalSpeed, context, "x1"),
-        _buildExecutionSpeedElement(ExecutionSpeed.doubleSpeed, context, "x2"),
-      ],
-    );
-  }
-
-  Widget _buildExecutionSpeedElement(
-      ExecutionSpeed speed, BuildContext context, String label) {
-    final segmentController = Provider.of<SegmentController>(context);
-    bool currentSpeedIsSelected =
-        segmentController.currentExecutionSpeed == speed;
-    return GestureDetector(
-      onTap: () => segmentController.changeExecutionSpeed(speed),
-      child: Container(
-        color: currentSpeedIsSelected
-            ? const Color.fromARGB(255, 238, 190, 99)
-            : const Color.fromARGB(255, 201, 159, 82),
-        height: 30,
-        width: 50,
-        child: Center(
-          child: Text(label),
-        ),
-      ),
+      children: speeds.map((speedData) {
+        bool currentSpeedIsSelected =
+            segmentController.currentExecutionSpeed == speedData.speed;
+        return InkWell(
+          onTap: () => segmentController.changeExecutionSpeed(speedData.speed),
+          child: Container(
+            color: currentSpeedIsSelected
+                ? const Color.fromARGB(255, 238, 190, 99)
+                : const Color.fromARGB(255, 201, 159, 82),
+            height: 30,
+            width: 50,
+            child: Center(
+              child: Text(speedData.label),
+            ),
+          ),
+        );
+      }).toList(),
     );
   }
 }
