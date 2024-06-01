1f067f7586846ea046d5d9df805dcaea22f6f758
LukasAppAkademie <lukas.buck@app-akademie.com>
2024-06-01 13:26:39 +0200

    litte design fix

--- lib/seven_segment_display/presentation/seven_segment_display_page.dart
+++ lib/seven_segment_display/presentation/seven_segment_display_page.dart
@@ -23,82 +23,84 @@ class _SevenSegmentDisplayPageState extends State<SevenSegmentDisplayPage> {
 
     return Scaffold(
       backgroundColor: Colors.grey,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const SizedBox(height: 30),
-          const RamValueDisplay(),
-          const SizedBox(height: 50),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                  padding: const EdgeInsets.all(16.0),
-                  decoration: BoxDecoration(border: Border.all()),
-                  child: const Digit()),
-            ],
-          ),
-          const SizedBox(height: 30),
-          const ExecutionSpeedSwitch(),
-          const SizedBox(height: 30),
-          Switch(
-            value: segmentController.executeInLoop,
-            onChanged: (bool value) {
-              segmentController.changeLoopStatus(value);
-            },
-          ),
-          const SizedBox(height: 30),
-          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-            const Text("Scripts:"),
-            const SizedBox(width: 15),
-            ...segmentController.scripts.map((model) {
-              return Text("${model.scriptName},");
-            }),
-          ]),
-          const SizedBox(height: 30),
-          Center(
-            child: SizedBox(
-              width: 300,
-              child: TextField(
-                controller: controller,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(height: 30),
+            const RamValueDisplay(),
+            const SizedBox(height: 50),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                    padding: const EdgeInsets.all(16.0),
+                    decoration: BoxDecoration(border: Border.all()),
+                    child: const Digit()),
+              ],
+            ),
+            const SizedBox(height: 30),
+            const ExecutionSpeedSwitch(),
+            const SizedBox(height: 30),
+            Switch(
+              value: segmentController.executeInLoop,
+              onChanged: (bool value) {
+                segmentController.changeLoopStatus(value);
+              },
+            ),
+            const SizedBox(height: 30),
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              const Text("Scripts:"),
+              const SizedBox(width: 15),
+              ...segmentController.scripts.map((model) {
+                return Text("${model.scriptName},");
+              }),
+            ]),
+            const SizedBox(height: 30),
+            Center(
+              child: SizedBox(
+                width: 300,
+                child: TextField(
+                  controller: controller,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                  ),
+                  minLines: 3,
+                  maxLines: 8,
                 ),
-                minLines: 3,
-                maxLines: 8,
               ),
             ),
-          ),
-          const SizedBox(height: 30),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              OutlinedButton(
-                onPressed: () => segmentController
-                    .executeCommands(controller.text.split('\n')),
-                child: const Text("EXECUTE"),
-              ),
-              const SizedBox(width: 15),
-              OutlinedButton(
-                onPressed: () => segmentController.reset(),
-                child: const Text("RESET"),
-              ),
-              const SizedBox(width: 15),
-              OutlinedButton(
-                onPressed: () {
-                  showDialog(
-                    context: context,
-                    builder: (context) {
-                      return AddScriptAlertDialog(
-                          scriptLines: controller.text.split('\n'));
-                    },
-                  );
-                },
-                child: const Text("ADD SCRIPT"),
-              ),
-            ],
-          ),
-        ],
+            const SizedBox(height: 30),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                OutlinedButton(
+                  onPressed: () => segmentController
+                      .executeCommands(controller.text.split('\n')),
+                  child: const Text("EXECUTE"),
+                ),
+                const SizedBox(width: 15),
+                OutlinedButton(
+                  onPressed: () => segmentController.reset(),
+                  child: const Text("RESET"),
+                ),
+                const SizedBox(width: 15),
+                OutlinedButton(
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (context) {
+                        return AddScriptAlertDialog(
+                            scriptLines: controller.text.split('\n'));
+                      },
+                    );
+                  },
+                  child: const Text("ADD SCRIPT"),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
